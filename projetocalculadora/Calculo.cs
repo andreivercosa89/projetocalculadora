5cb4b7a9d0070c02678e9c14a245bd41c2011dca
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-10-29 21:15:18 -0400

    calcular dois numeros

---
+++ projetocalculadora/Calculo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace projetocalculadora
+{
+    public class Calculo
+    {
+            private int num1, num2, resultado, teste;
+
+        public void definirNum1(int valor1)
+        {
+            num1 = valor1;
+        }
+
+        public void definirNum2(int valor2)
+        {
+            num2 = valor2;
+        }
+
+        public void realizarOperacao (int sinal){
+            teste = sinal;
+            switch (sinal)
+                {
+                    case 1:
+                        resultado = (num1 + num2);
+                        break;
+
+                    case 2:
+                        resultado = (num1 - num2);
+                        break;
+
+                    case 3:
+                        resultado = (num1 * num2);
+                        break;
+
+                    case 4:
+                        resultado = (num1 / num2);
+                        break;
+
+                    default:
+
+                        break;
+
+                }
+
+        }
+
+
+        public int resul()
+        {
+            return resultado;
+        }
+
+        public int test()
+        {
+            return teste;
+        }
+    }
+}
+
---
+++ projetocalculadora/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace projetocalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculo calc = new Calculo();
+
+            Console.WriteLine("Informe O 1o numeros");
+            calc.definirNum1(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Informe O 2o numeros");
+            calc.definirNum2(Int32.Parse(Console.ReadLine()));
+
+
+            Console.WriteLine("Informe a operacao:\n\n1 - Soma;\n2 - Subtracao;\n3 - Multiplicacao;\n4 - Divisao.");
+            calc.realizarOperacao(Int32.Parse(Console.ReadLine()));
+
+            if(calc.test()>= 0 & calc.test() <= 4){
+                Console.WriteLine("\nO resultado e: " + calc.resul());
+            }
+            else{
+                Console.WriteLine("Operacao incorreta");
+            }
+
+
+        }
+            
+    }
+}
